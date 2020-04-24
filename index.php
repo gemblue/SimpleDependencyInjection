774cbbddb3f5f60efa2769afc51f397fbaaa35d0
oriza <ahmadoriza@gmail.com>
2020-04-24 23:48:19 +0700

    Put User sample class

--- index.php
+++ index.php
@@ -1,3 +1,24 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
+
+require 'Container.php';
+
+/**
+ * Usage
+ */
+class User {
+    
+    public $db;
+
+    public function __construct(Db $db, $config = 'NULL', Cache $cache) 
+    {    
+        $this->db = $db;
+
+        echo "User with config {$config} was constructed <br/>";
+    }
+
+    public function register($name) {
+        $this->db->execute("INSERT INTO user SET name={$name}");
+    }
+}
