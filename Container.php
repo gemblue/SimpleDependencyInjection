329980325e13e9f6a37c1df5edb0d4c18b1ff2c0
oriza <ahmadoriza@gmail.com>
2020-04-24 23:46:52 +0700

    Create resolver method

--- Container.php
+++ Container.php
@@ -8,5 +8,23 @@
  * @author Gemblue
  */
 class Container {
+    
+    /**
+     * Resolve class, instantiate automate by reflection.
+     *
+     * @return object
+     */
+    public function resolve(string $className) 
+    {
+        // Create a reflection.
+        $reflector = new ReflectionClass($className);
+        
+        // Get it's constructor.
+        $constructor = $reflector->getConstructor();
+        
+        // Search dependency and resolve. By constructor parameters.
+        $args = $this->getDependencies($constructor->getParameters());
 
+        return $reflector->newInstanceArgs($args); 
+    }
 }
